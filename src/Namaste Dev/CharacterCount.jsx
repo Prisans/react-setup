8362d1948b6d392fefd30ade0d89f6bfe7435038
Prisans <124236508+Prisans@users.noreply.github.com>
2026-02-07 19:04:41 +0530

    character count fixed

--- src/Namaste Dev/CharacterCount.jsx
+++ src/Namaste Dev/CharacterCount.jsx
@@ -4,12 +4,10 @@ const CharacterCount = () => {
     const [inpData,setInpData]=useState("")
     const [inpDataLen,setInpDataLen] = useState(0)
     const [maxLen,setMaxLen] = useState(0)
-    const [warning,setWarning] = useState(false)
 
     function handleChange(e){
         setInpData(e.target.value)
         setInpDataLen(e.target.value.length)
-        e.target.value.length >= maxLen ? setWarning(true) : setWarning(false)
     }
     // console.log("inp data is" , inpData)
   return (
@@ -19,7 +17,7 @@ const CharacterCount = () => {
         <label>Max limit <input onChange={(e)=>setMaxLen(e.target.value)} value={maxLen} type="text" min="0" max="1000" /></label>
         <p>{`${inpDataLen} / ${maxLen}`}</p>
         <textarea value={inpData} onChange={handleChange} name="" id="" cols="30" rows="10" placeholder='enter here...'></textarea>
-        {warning && <p>Count limit exceeded</p>}
+        {inpDataLen>=maxLen && <p>Count limit exceeded</p>}
     </div>
 
   )
